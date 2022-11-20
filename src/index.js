d434da8037d852094f06a9616af95d515ae204cf
TryHungury <mukolanadebernuy@gmail.com>
2022-11-20 16:13:58 +0200

    global update file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import {basiclightbox} from "basiclightbox"
+import * as basicLightbox from 'basiclightbox'
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600" alt="Котик">
+`)
+
+instance.show()
