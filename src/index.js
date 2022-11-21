b1d9066d75eb362740ebb5dd1307d0af1b2a71b7
TryHungury <mukolanadebernuy@gmail.com>
2022-11-21 17:15:06 +0200

    Task complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,135 @@
-// import {basiclightbox} from "basiclightbox"
-import * as basicLightbox from 'basiclightbox'
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600" alt="Котик">
-`)
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-instance.show()
+const lightbox = new SimpleLightbox('.gallery a', {animationSpeed: 400, captionsData: 'alt',captionDelay: 250});
+
+const ref = {
+    searchForm: document.querySelector(".search-form"),
+    searchInput: document.querySelector(".search-form__input"),
+    searchButton: document.querySelector(".search-form__btn"),
+    galleryRef: document.querySelector(".gallery"),
+    btnLoad: document.querySelector('.load-more')
+}
+
+let page = 1;
+let inputValue = "";
+const BASE_URL = "https://pixabay.com/api/";
+const MY_KEY = "key=31470169-9c9cb372459e41628308e9796"
+const IMG_TYPE = "image_type=photo&oriental=horizontal&safeswarch=true"
+const QUANTITY_PER_PAGE = `per_page=${40}`
+
+ref.searchForm.addEventListener("submit", makeSubmit)
+ref.btnLoad.addEventListener('click', loadMore)
+
+function makeSubmit(e) {
+    preventDefaultOn(e);
+    clearOldMarkup();
+
+    page = 1;
+
+    inputValue = writeInputValue(e);
+    if (inputValue === "") {
+        Notiflix.Notify.failure('Pls typing anything...')
+    } else {
+
+        makeResult(page);
+    }
+    // console.log(page)
+}
+
+function loadMore(e) {
+    preventDefaultOn(e);
+    page += 1;
+
+    makeResult(page)
+}
+
+function preventDefaultOn(event) {
+    event.preventDefault();
+}
+
+function clearOldMarkup() {
+    ref.galleryRef.innerHTML = "";
+}
+
+function writeInputValue(event) {
+    return event.srcElement[0].value;
+}
+
+async function makeResult(page) {
+    const data = await fetchAxios(page);
+    const markup = await makeMarkup(data);
+    lightbox.refresh();
+}
+
+function makeMarkup(data) {
+    // console.log(data.hits[0])
+    const allTotalHits = data.totalHits;
+    if (page === 1) {
+        if (allTotalHits === 0) {
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+            btnLoadOn()
+        } else {
+            Notiflix.Notify.success(`Yuhu, we found ${allTotalHits} images`);
+            btnLoadOff()
+        }
+    } else {
+        const calculateQuantityImg = allTotalHits - 40 * (page-1);
+        if (calculateQuantityImg < 40) {
+            Notiflix.Notify.failure('Were sorry, but youve reached the end of search results.')
+            btnLoadOn()
+        } else {
+            Notiflix.Notify.success(`Yuhu, we found ${calculateQuantityImg} images`);
+            btnLoadOff()
+        }
+        // console.log(calculateQuantityImg)
+    }
+
+    const markup = data.hits.map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads})=>{
+        // console.log(largeImageURL)
+        return `
+        <div class="photo-card">
+        <div class="thumb">
+            <a class = gallery__item href = "${largeImageURL}">
+                <img class="card-img" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+            </a>
+        </div>
+            <div class="info">
+                <p class="info-item">
+                    <b class="info-item-span">Likes:</b>
+                    ${likes}
+                </p>
+                <p class="info-item">
+                    <b class="info-item-span">Views:</b>
+                    ${views}
+                </p>
+                <p class="info-item">
+                    <b class="info-item-span">Comments:</b>
+                    ${comments}
+                </p>
+                <p class="info-item">
+                    <b class="info-item-span">Downloads:</b>
+                    ${downloads}
+                </p>
+            </div>
+        </div>`
+    })
+    return ref.galleryRef.insertAdjacentHTML('beforeend', markup.join(''));
+}
+
+function btnLoadOn() {
+    ref.btnLoad.classList.add('visually-hidden');
+}
+
+function btnLoadOff() {
+    ref.btnLoad.classList.remove('visually-hidden')
+}
+
+async function fetchAxios(page) {
+    let urlObject = await axios.get(`${BASE_URL}?${MY_KEY}&q=${inputValue}&${IMG_TYPE}&${QUANTITY_PER_PAGE}&page=${page}`);
+    const data = urlObject.data;
+    
+    return data;
+}
